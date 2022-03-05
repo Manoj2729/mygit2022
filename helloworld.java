0f29e6db8163f0830a672735fcb2d5c60e8955ad
manu <manu143@gmail.com>
2022-03-05 04:06:09 -0800

    hai

--- helloworld.java
+++ helloworld.java
@@ -3,10 +3,4 @@ class HelloWorld {
     public static void main(String args[]) {
         System.out.println("Hello World!");
     }
-=======
-class HelloWorld {
-    public static void main(String args[]) {
-        System.out.println("Hello World!");
-    }
->>>>>>> 229ef793fd72c435062b3a0dfc45527dbbc8c224
-}
+
